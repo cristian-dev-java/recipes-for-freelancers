b9c0a8dfe7f3c31c41f17baa13d80953e6402bdd
DESKTOP-O8JE48T\Criss <cristiancamilomunozarango@gmail.com>
2022-10-27 21:38:14 -0500

    Se crea clase para manejar mensajes personalizados, y clase para manejar las constantes.

---
+++ src/main/java/com/myprojects/apiparameters/util/ParametersConstants.java
@@ -0,0 +1,14 @@
+package com.myprojects.apiparameters.util;
+
+public class ParametersConstants {
+
+    private ParametersConstants() {
+    }
+
+    /* Messages codes */
+    public static final String INTERNAL_SERVER_ERROR = "PA001";
+    public static final String BAD_REQUEST = "PA002";
+    public static final String SUCCESSFUL = "PA003";
+    public static final String REQUIRED_FIELD = "PA004";
+
+}
